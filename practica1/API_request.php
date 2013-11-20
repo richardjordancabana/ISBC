38d63672822dfd4774958f843f2bd3ad0eb1d0eb
rchampa <ricardocb48@gmail.com>
2013-11-20 21:13:17 +0100

    Errors with hashtags and mentions has been solved

--- practica1/API_request.php
+++ practica1/API_request.php
@@ -3,9 +3,9 @@ header('Content-Type: text/html; charset=UTF-8');
 ini_set('display_errors', 1);
 //twitter
 require_once('TwitterAPIExchange.php');
-//lexicón
+//lexicï¿½n
 require_once 'procesamiento.php';	
-//gráfico
+//grï¿½fico
 require_once 'graph.php';
 
 $settings = array(
@@ -17,9 +17,14 @@ $settings = array(
 
 ini_set('max_execution_time', 500);
 
-$analize = $_POST['analize'];//tr: jesus_pkmd,RicTheImpaler,#LeyAnti15M
+$analize = $_POST['analize'];
 $type = $_POST['type'];
 $limit = $_POST['limit'];
+
+//XXX
+//$analize = "@jesus_pkmd";//tr: jesus_pkmd,RicTheImpaler,#LeyAnti15M
+//$type = "mention";//username,mention,hashtag
+//$limit = "25";
 
 $lexicon = cargar("lexicon.txt");
 $num_tweets = 0;
@@ -30,41 +35,112 @@ $valor_total = 0;
 $lista_tweets = '';
 $cont_pags = 0;
 
+$error = false;
+
 do{
+    if($num_tweets>=$limit)
+        break;
+    
     if(isset($tweet['id_str'])){
         $tweets = getTweets($settings,$analize,$type,'&max_id='.$tweet['id_str']);
     }
     else{
         $tweets = getTweets($settings,$analize,$type);
     }
-    foreach($tweets as $tweet) {
-		$valor = calculaValor($lexicon, $tweet['text']);
-		if ($valor > 0) { //Tweet Positivo
-			$num_pos ++;
-		}
-		else if ($valor < 0) { //Tweet Negativo
-			$num_neg ++;
-		}
-		else { //Tweet Neutro
-			$num_neutro ++;
-		}
-		$valor_total = $valor_total + $valor; //Cálculo del valor total
-		$lista_tweets .= $num_tweets+1 .' ['.$valor.'] '.$tweet['text']."<br>";
-        $num_tweets++;		
+    
+    if(isset($tweets['errors'][0]['message'])){
+        $error = true;
+        break;
     }
-	$cont_pags++;
+    
+    if($type==="username"){
+        
+        foreach($tweets as $tweet) {
+            if($num_tweets>=$limit)
+                break;
+            $valor = calculaValor($lexicon, $tweet['text']);
+            if ($valor > 0) { //Tweet Positivo
+                    $num_pos ++;
+            }
+            else if ($valor < 0) { //Tweet Negativo
+                    $num_neg ++;
+            }
+            else { //Tweet Neutro
+                    $num_neutro ++;
+            }
+            $valor_total = $valor_total + $valor; //CÃ¡lculo del valor total
+            $lista_tweets .= $num_tweets+1 .' ['.$valor.'] '.$tweet['text']."<br>";
+            $num_tweets++;	
+        }
+        
+    }
+    else if($type==="mention"){//hashtag
+        $screen_name = substr($analize, 1);
+        foreach($tweets['statuses'] as $tweet) {
+            
+            if($num_tweets>=$limit)
+                break;
+            
+            if($tweet['user']['screen_name']===$screen_name){
+                $valor = calculaValor($lexicon, $tweet['text']);
+                if ($valor > 0) { //Tweet Positivo
+                        $num_pos ++;
+                }
+                else if ($valor < 0) { //Tweet Negativo
+                        $num_neg ++;
+                }
+                else { //Tweet Neutro
+                        $num_neutro ++;
+                }
+                $valor_total = $valor_total + $valor; //CÃ¡lculo del valor total
+                $lista_tweets .= $num_tweets+1 .' ['.$valor.'] '.$tweet['text']."<br>";
+                $num_tweets++;
+            }
+            
+        }
+    }//hashtag
+    else{
+        foreach($tweets['statuses'] as $tweet) {
+            
+            if($num_tweets>=$limit)
+                break;
+            
+            $valor = calculaValor($lexicon, $tweet['text']);
+            if ($valor > 0) { //Tweet Positivo
+                    $num_pos ++;
+            }
+            else if ($valor < 0) { //Tweet Negativo
+                    $num_neg ++;
+            }
+            else { //Tweet Neutro
+                    $num_neutro ++;
+            }
+            $valor_total = $valor_total + $valor; //CÃ¡lculo del valor total
+            $lista_tweets .= $num_tweets+1 .' ['.$valor.'] '.$tweet['text']."<br>";
+            $num_tweets++;	
+        }
+    }
+   
+    $cont_pags++;
 }while(isset($tweet['id_str']) && $num_tweets<=$limit);
 
-//ECHOS/////////////////
-echo "<br>".'Cargados '.$num_tweets.' tweets de '.$cont_pags.' paginas.'."<br>"."<br>";
-echo 'Numero de Tweets positivos: '.$num_pos."<br>";
-echo 'Numero de Tweets negativos: '.$num_neg."<br>";
-echo 'Numero de Tweets neutros: '.$num_neutro."<br>";
-echo 'Valor total: '.$valor_total."<br>";
-echo drawChart($num_pos, $num_neg, $num_neutro);
-echo 'Lista de tweets cargados: '."<br>";
-echo $lista_tweets;
-////////////////////////
+
+if($error===true){
+    echo "The ".$analize." does not exists.";
+}
+else{
+
+    //ECHOS/////////////////
+    echo "<br>".'Cargados '.$num_tweets.' tweets de '.$cont_pags.' consultas.'."<br>"."<br>";
+    echo 'Numero de Tweets positivos: '.$num_pos."<br>";
+    echo 'Numero de Tweets negativos: '.$num_neg."<br>";
+    echo 'Numero de Tweets neutros: '.$num_neutro."<br>";
+    echo 'Valor total: '.$valor_total."<br>";
+    echo drawChart($num_pos, $num_neg, $num_neutro);
+    echo 'Lista de tweets cargados: '."<br>";
+    echo $lista_tweets;
+    ////////////////////////
+}
 
 
 /** Perform a GET request and echo the response **/
@@ -77,9 +153,9 @@ function getTweets($settings,$name,$type,$maxid=''){
         $getfield = '?screen_name='.$name.$maxid;
     }
     else if($type==="mention"){
-        $url = 'https://api.twitter.com/1.1/statuses/mentions_timeline.json';
+        $url = 'https://api.twitter.com/1.1/search/tweets.json';
         $name = substr($name, 1);
-        $getfield = '?screen_name='.$name.$maxid;
+        $getfield = '?q='.$name.$maxid;
     }
     else{//hashtag
         $url = 'https://api.twitter.com/1.1/search/tweets.json';
