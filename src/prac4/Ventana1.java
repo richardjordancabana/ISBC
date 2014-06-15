c6ed9a8e03d9372861ca9c59b0b54e1c21cfc7f2
Jesus Lopez <jesuslopezparkour@gmail.com>
2014-06-15 23:03:42 +0200

    base de proyecto funcionando

--- src/prac4/Ventana1.java
+++ src/prac4/Ventana1.java
@@ -6,6 +6,8 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTabbedPane;
+import java.awt.Color;
 
 public class Ventana1 extends JFrame {
 
@@ -15,10 +17,12 @@ public class Ventana1 extends JFrame {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+		Conector conector = new Conector();
+		final JPanel panOnt = conector.getPanelOntologia();
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Ventana1 frame = new Ventana1();
+					Ventana1 frame = new Ventana1(panOnt);
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -30,13 +34,20 @@ public class Ventana1 extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public Ventana1() {
+	public Ventana1(JPanel panOnt) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 680, 550);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JPanel panelOntologia = panOnt;
+		contentPane.add(panelOntologia, BorderLayout.WEST);
+		
+		JTabbedPane tabbedPane1 = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane1.setBackground(Color.LIGHT_GRAY);
+		contentPane.add(tabbedPane1, BorderLayout.CENTER);
 	}
 
 }
