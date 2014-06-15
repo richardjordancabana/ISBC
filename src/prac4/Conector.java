8968f377b499aec678f37c6f47beee3548bbc787
Jesus Lopez <jesuslopezparkour@gmail.com>
2014-06-15 22:16:06 +0200

    pr4

---
+++ src/prac4/Conector.java
@@ -0,0 +1,9 @@
+package prac4;
+
+import es.ucm.fdi.gaia.ontobridge.OntoBridge;
+import es.ucm.fdi.gaia.ontobridge.OntologyDocument;
+import es.ucm.fdi.gaia.ontobridge.test.gui.PnlSelectInstance;
+
+public class Conector {
+
+}
