4ef29ebca9e1d7b0523067b8b5852b6e3a0a0eda
richardjordancabana <pichipichi310@hotmail.com>
2014-06-15 22:38:26 +0200

    intento

--- src/prac4/Conector.java
+++ src/prac4/Conector.java
@@ -1,9 +1,35 @@
 package prac4;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import javax.swing.JPanel;
 
 import es.ucm.fdi.gaia.ontobridge.OntoBridge;
 import es.ucm.fdi.gaia.ontobridge.OntologyDocument;
 import es.ucm.fdi.gaia.ontobridge.test.gui.PnlSelectInstance;
 
-public class Conector {
 
+public class Conector {
+	
+	//nuestra url
+	private static final String URL = "http://www.owl-ontologies.com/Ontology1402519238.owl";
+	
+	private OntoBridge ontologia;
+	
+	
+	public Conector(){
+		ontologia = new OntoBridge();
+		ontologia.initWithPelletReasoner();// usaremos el razonador de pellet
+		//nuestro fichero owl
+		OntologyDocument mainOnto = new OntologyDocument(URL,"file:Ejercicio2.owl");
+		ArrayList<OntologyDocument> subOntologies = new ArrayList<OntologyDocument>();	
+		ontologia.loadOntology(mainOnto, subOntologies, false);
+		
+	}
+	
+	public JPanel getPanelOntologia() {
+		return new PnlSelectInstance(ontologia);
+	}
+	
+	
 }
